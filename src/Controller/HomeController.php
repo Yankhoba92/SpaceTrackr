63d2ebb03b72d02cde50a906cb48005bd213bccf
Yankhoba <bagayoko.yan@gmail.com>
2024-02-14 14:24:49 +0100

    🔨 Mise en place de la barre de rechcherche par title

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,9 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -13,6 +14,22 @@ class HomeController extends AbstractController
     public function index(RoomRepository $roomRepository): Response
     {
         $rooms = $roomRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'rooms' => $rooms,
+        ]);
+    }
+
+    #[Route('/rooms/search', name: 'search_rooms')]
+    public function search(Request $request, RoomRepository $roomRepository): Response
+    {
+        $query = $request->query->get('query');
+        if (!$query) {
+            // Si aucune requête n'est fournie, redirigez vers la page d'index des salles
+            return $this->redirectToRoute('app_room');
+        }
+
+        // Utilisez le repository des salles pour rechercher les salles par nom
+        $rooms = $roomRepository->findByName($query);
         return $this->render('home/index.html.twig', [
             'rooms' => $rooms,
         ]);
